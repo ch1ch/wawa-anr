14b026a978c2dc84c7d6fa7e6f308f334442e304
SharerMax <mdcw1103@gmail.com>
2017-11-20 00:22:11 +0800

    add some improvement

--- app/src/main/kotlin/cn/legendream/wawa/app/WaWaApplication.kt
+++ app/src/main/kotlin/cn/legendream/wawa/app/WaWaApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import cn.legendream.wawa.BuildConfig
 import cn.legendream.wawa.app.user.UserManager
 import com.shuyu.gsyvideoplayer.GSYVideoManager
+import com.shuyu.gsyvideoplayer.model.VideoOptionModel
 import com.wilddog.wilddogcore.WilddogApp
 import com.wilddog.wilddogcore.WilddogOptions
 import timber.log.Timber
@@ -27,6 +28,19 @@ class WaWaApplication : Application() {
         } else {
             GSYVideoManager.instance().setLogLevel(IjkMediaPlayer.IJK_LOG_WARN)
         }
+
+        val ijkOptions = ArrayList<VideoOptionModel>()
+        ijkOptions.add(VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "start-on-prepared", 0))
+        ijkOptions.add(
+            VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "http-detect-range-support", 0))
+        ijkOptions.add(VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_CODEC, "skip_loop_filter", 48))
+        ijkOptions.add(VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_CODEC, "skip_frame", 8))
+        ijkOptions.add(VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzemaxduration", 100))
+        ijkOptions.add(VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", 10240))
+        ijkOptions.add(VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "flush_packets", 1))
+        ijkOptions.add(VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "packet-buffering", 0))
+        ijkOptions.add(VideoOptionModel(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 1))
+        GSYVideoManager.instance().optionModelList = ijkOptions
 
         UserManager.init(this)
         val options = WilddogOptions.Builder().setSyncUrl(
